c23a8c5c9fd7f1fe899aebc532fd963db6066a1c
Jovan <jovannik88@gmai.com>
2025-12-13 20:24:41 +0700

    Separating js file from the hmtl file for Login Page, Course Page and Contact page

---
+++ contact.js
@@ -0,0 +1,9 @@
+document.querySelector(".p3").addEventListener("click", () => {
+    window.location.href = "Course Page.html";
+});
+document.querySelector(".p4").addEventListener("click", () => {
+    window.location.href = "Contact.html";
+});
+document.querySelector(".p2").addEventListener("click", () => {
+    window.location.href = "about.html";
+});
